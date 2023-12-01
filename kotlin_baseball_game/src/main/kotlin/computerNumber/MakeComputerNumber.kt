837e93c990a27f8dc8b722cd53311dc503f30e4c
sungsik-git <kss950708@gamil.com>
2023-12-01 20:27:48 +0900

    refact: 컴퓨터 랜덤숫자 클래스 분리

---
+++ kotlin_baseball_game/src/main/kotlin/computerNumber/MakeComputerNumber.kt
@@ -0,0 +1,22 @@
+package computerNumber
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class MakeComputerNumber {
+    companion object{
+        const val SIZE_OF_COMPUTER_NUMBER = 3
+        const val MINIMUM_VALUE_OF_RANDOM_NUMBER = 1
+        const val MAXIMUM_VALUE_OF_RANDOM_NUMBER = 9
+    }
+
+    fun makeComputerNumber() : MutableList<Int> {
+        val computerNumber = mutableListOf<Int>()
+        while (computerNumber.size < SIZE_OF_COMPUTER_NUMBER) {
+            val randomNumber = Randoms.pickNumberInRange(MINIMUM_VALUE_OF_RANDOM_NUMBER, MAXIMUM_VALUE_OF_RANDOM_NUMBER)
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber)
+            }
+        }
+        return computerNumber
+    }
+}
